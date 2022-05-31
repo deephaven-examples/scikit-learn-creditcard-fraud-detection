fc2c81a3242d3e1fa30b677693692b261d491fda
JJ Brosnan <84038776+jjbrosnan@users.noreply.github.com>
2022-05-31 12:54:55 -0400

    Update real_time_fraud_detection.py

--- data/notebooks/real_time_fraud_detection.py
+++ data/notebooks/real_time_fraud_detection.py
@@ -1,11 +1,11 @@
-start_time = dbtu.convertDateTime("2021-11-17T12:00:00 NY")
-end_time = dbtu.convertDateTime("2021-11-17T16:01:00 NY")
+start_time = dbtu.to_datetime("2021-11-17T12:00:00 NY")
+end_time = dbtu.to_datetime("2021-11-17T16:01:00 NY")
 
 # Replay the test_data table
-test_data_replayer = Replayer(start_time, end_time)
-creditcard_live = test_data_replayer.replay(test_data, "TimeStamp")
+test_data_replayer = TableReplayer(start_time, end_time)
+creditcard_live = test_data_replayer.add_table(test_data, "TimeStamp")
 test_data_replayer.start()
-creditcard_live = creditcard_live.view("Time", "V4", "V12", "V14", "Amount", "Class")
+creditcard_live = creditcard_live.view(["Time", "V4", "V12", "V14", "Amount", "Class"])
 
 # A function to place new observations into our existing clusters
 def dbscan_predict(X_new):
@@ -22,7 +22,7 @@ def dbscan_predict(X_new):
 
 # A function to gather data from a Deephaven table into a NumPy array
 def table_to_numpy(rows, cols):
-    return gather.table_to_numpy_2d(rows, cols, dtype = np.double)
+    return gather.table_to_numpy_2d(rows, cols, np_type = np.double)
 
 # A function to scatter data back into an output table
 def scatter(data, idx):
@@ -36,7 +36,7 @@ predicted_fraud_live = learn.learn(
     batch_size = 1000
 )
 
-dbscan_correct_valid = predicted_fraud_live.where("PredictedClass == 0 && Class == 0")
-dbscan_correct_fraud = predicted_fraud_live.where("PredictedClass == 1 && Class == 1")
-dbscan_false_positive = predicted_fraud_live.where("PredictedClass == 1 && Class == 0")
-dbscan_false_negative = predicted_fraud_live.where("PredictedClass == 0 && Class == 1")
+dbscan_correct_valid = predicted_fraud_live.where(["PredictedClass == 0 && Class == 0"])
+dbscan_correct_fraud = predicted_fraud_live.where(["PredictedClass == 1 && Class == 1"])
+dbscan_false_positive = predicted_fraud_live.where(["PredictedClass == 1 && Class == 0"])
+dbscan_false_negative = predicted_fraud_live.where(["PredictedClass == 0 && Class == 1"])
